f9eefa8bc4179aa0cef6dd18674b24ff7aab8e1b
Antoine Cheynet <antoine@typedin.dev>
2021-07-19 22:16:54 +0200

    use plugin instead of focus function

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,6 +1,7 @@
-require("config.compe")
 require("config.colorizer")
+require("config.compe")
 require("config.format")
 require("config.gitsigns")
 require("config.lspsaga")
 require("config.lualine")
+require("config.shade")
