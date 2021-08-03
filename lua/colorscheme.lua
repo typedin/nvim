1ebd94169fa1ff288f2ac1a4ec4012534dc0bac9
Antoine Cheynet <antoine@typedin.dev>
2021-08-03 13:16:43 +0200

    removed dead code

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,23 +1,7 @@
-vim.opt.termguicolors = true
--- vim.opt.background = "dark"
--- https://github.com/shaunsingh/nord.nvim
 -- the following settings are overriden in after/plugin/color.vim
---[[ vim.g.nord_contrast = true
-vim.g.nord_borders = false
-vim.g.nord_disable_background = true
-require('nord').set() ]]
-vim.cmd([[
-  "colorscheme nord
-]])
+vim.opt.termguicolors = true
 vim.o.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme gruvbox]])
-vim.cmd([[
-  augroup nord-theme-overrides
-  autocmd!
-  " Use 'nord6' as foreground color for all comments.
-  " autocmd ColorScheme nord highlight Comment ctermfg=15 guifg=#FFEFF4
-  augroup END
-]])
 
 vim.cmd([[
   augroup BgHighlight
