9b44a5e24526142aab798d12a303b8a1ad81b7c5
Antoine Cheynet <antoine@typedin.dev>
2021-08-10 17:27:23 +0200

    renaming

--- lua/helpers/mappers.lua
+++ lua/helpers/mappers.lua
@@ -2,19 +2,20 @@ local M = {}
 
 function M.local_buffer_mapper(mappings)
     local nnoremap = function(lhs, rhs)
-        local _opts = {noremap = true, silent = true}
-        vim.api.nvim_buf_set_keymap(0, 'n', lhs, rhs, _opts)
+        local options = {noremap = true, silent = true}
+        vim.api.nvim_buf_set_keymap(0, 'n', lhs, rhs, options)
     end
 
     for lhs, rhs in pairs(mappings) do nnoremap(lhs, rhs) end
 end
 
 function M.mode_mapper(mode, mappings)
-    local _opts = {noremap = true, silent = true}
     local map_function = function(lhs, rhs)
-        vim.api.nvim_set_keymap(mode, lhs, rhs, _opts)
+        local options = {noremap = true, silent = true}
+        vim.api.nvim_set_keymap(mode, lhs, rhs, options)
     end
 
     for lhs, rhs in pairs(mappings) do map_function(lhs, rhs) end
 end
+
 return M
