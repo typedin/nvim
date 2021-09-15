3e6c706fe7e03195e08f8add18dc0b31102199b3
Antoine Cheynet <antoine@typedin.dev>
2021-09-15 12:54:08 +0200

    lsp: use tailwind css

---
+++ lua/config-lsp/lsp-tailwind.lua
@@ -0,0 +1,13 @@
+require("lspconfig").tailwindcss.setup {
+    cmd = {
+        DATA_PATH ..
+            "/lspinstall/tailwindcss/node_modules/.bin/tailwindcss-language-server",
+        "--stdio"
+    },
+    filetypes = {
+        "vue", "html", "javascriptreact", "javascript.jsx", "typescriptreact"
+    },
+    root_dir = require("lspconfig/util").root_pattern("package.json",
+                                                      "tsconfig.json",
+                                                      "jsconfig.json", ".git")
+}
