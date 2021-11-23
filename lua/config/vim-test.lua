b57f985abe5fa70dc60e39aa9f6058c3486495c2
Antoine Cheynet <antoine@typedin.dev>
2021-11-23 20:53:14 +0100

    basecoding

--- lua/config/vim-test.lua
+++ lua/config/vim-test.lua
@@ -10,7 +10,7 @@ vim.api.nvim_set_var("test#strategy", 'vimux')
 
 vim.cmd([[
 " set current path when vim loads up
-let g:test#project_root = finddir('.git/..', expand('%:p:h').';') 
+let g:test#project_root = finddir('.git/..', expand('%:p:h').';')
 
 " initially empty status
 " let g:testing_status = ''
@@ -23,12 +23,12 @@ nnoremap <silent> t_ ^/function <CR>ewvt(:s/\%V /_/g<CR>jji<TAB>
 "nnoremap <silent>tcr :call ToggleStrategy()<CR>
 
 function! ToggleStrategy()
-	if g:test#strategy == "vimux"
-		let g:test#strategy = "neomake"
-	else
-		let g:test#strategy = "vimux"
-	endif
-	:echo "changed test strategy to: " . g:test#strategy
+    if g:test#strategy == "vimux"
+        let g:test#strategy = "neomake"
+    else
+        let g:test#strategy = "vimux"
+    endif
+:echo "changed test strategy to: " . g:test#strategy
 endfunction
 
 " use neomake for async running of tests
@@ -36,38 +36,38 @@ endfunction
 let g:neomake_open_list = 1
 
 augroup neomake_hook
-	au!
-	autocmd User NeomakeJobFinished call TestFinished()
-	autocmd User NeomakeJobStarted call TestStarted()
+    au!
+    autocmd User NeomakeJobFinished call TestFinished()
+    autocmd User NeomakeJobStarted call TestStarted()
 augroup END
 
 " Start test
 function! TestStarted() abort
-	let g:testing_status = 'Test ⌛'
-	:call PrintStatus()
+    let g:testing_status = 'Test ⌛'
+    :call PrintStatus()
 endfunction
 
 " Show message when all tests are passing
 function! TestFinished() abort
-	let context = g:neomake_hook_context
-	if context.jobinfo.exit_code == 0
-		let g:testing_status = 'Test ✅'
-		" test commit
-		execute '!(cd ' . g:test#project_root .'; git add . ; git commit -n -m "wip (tcr) ")'
-	endif
-	if context.jobinfo.exit_code == 1
-		let g:testing_status = 'Test ❌'
-		" test revert
-		:!git reset --hard
-	endif
-	:call PrintStatus()
+    let context = g:neomake_hook_context
+    if context.jobinfo.exit_code == 0
+        let g:testing_status = 'Test ✅'
+        " test commit
+        execute '!(cd ' . g:test#project_root .'; git add . ; git commit -n -m "wip (tcr) ")'
+    endif
+    if context.jobinfo.exit_code == 1
+        let g:testing_status = 'Test ❌'
+        " test revert
+        :!git reset --hard
+    endif
+    :call PrintStatus()
 endfunction
 
 function! TestStatus() abort
-	:call PrintStatus()
+    :call PrintStatus()
 endfunction
 
 function! PrintStatus()
-	:echo g:testing_status
+    :echo g:testing_status
 endfunction
 ]])
