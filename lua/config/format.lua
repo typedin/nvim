c0757a63eb03a75ecde8065ba63a003225da1941
Antoine Cheynet <antoine@typedin.dev>
2021-11-23 20:54:45 +0100

    formatting: got rid of eslint

--- lua/config/format.lua
+++ lua/config/format.lua
@@ -1,88 +1,57 @@
+-- Note: Lachlan Miller:
+-- delete the linter(s)
 -- https://github.com/mhartington/formatter.nvim/issues/31
--- this is where it is exmplained how to set many linters/formatters for one
+-- this is where it is explained how to set many linters/formatters for one
 -- filetype
 local c_lang = function()
     return {
-        exe = "clang-format",
         args = {},
-        stdin = true,
-        cwd = vim.fn.expand('%:p:h') -- Run clang-format in cwd of the file.
+        cwd = vim.fn.expand('%:p:h'), -- Run clang-format in cwd of the file.
+        exe = "clang-format",
+        stdin = true
     }
 end
-local prettier = function()
+local prettierConfig = function()
     return {
-        exe = "prettier",
         args = {"--stdin-filepath", vim.api.nvim_buf_get_name(0)},
+        exe = "prettier",
         stdin = true
     }
 end
-local json = function()
-    return {
-        exe = "jsonlint",
-        args = {"--stdin-kilename", vim.api.nvim_buf_get_name(0)},
-        stdin = true
+local formatterConfig = {
+    c = {c_lang},
+    cpp = {c_lang},
+    python = {
+        -- black
+        function() return {exe = "black", args = {"-"}, stdin = true} end
+    },
+    lua = {
+        -- luafmt
+        function()
+            return {exe = "lua-format", args = {"-i"}, stdin = true}
+        end
+    },
+    php = {
+        function()
+            return {exe = "php-cs-fixer", args = {"fix"}, stdin = false}
+        end
+    },
+    vue = {
+        function()
+            return {
+                exe = "prettier",
+                args = {
+                    "--stdin-filepath", vim.api.nvim_buf_get_name(0),
+                    "--double-quote", "--parser", "vue"
+                },
+                stdin = true
+            }
+        end
     }
-end
-local eslint = function()
-    return {
-        exe = "eslint_d",
-        args = {"--stdin-kilename", vim.api.nvim_buf_get_name(0)},
-        stdin = true
-    }
-end
-
-local markdown = function()
-    return {
-        exe = "markdownlint",
-        args = {"--fix", vim.api.nvim_buf_get_name(0)},
-        stdin = false
-    }
-end
-
-local css = function()
-    return {
-        exe = "stylelint",
-        args = {"--fix", vim.api.nvim_buf_get_name(0)},
-        stdin = false
-    }
-end
-
-require('formatter').setup({
-    logging = true,
-    filetype = {
-        c = {c_lang},
-        cpp = {c_lang},
-        css = {css},
-        typescript = {prettier, eslint},
-        javascript = {prettier, eslint},
-        javascriptreact = {prettier, eslint},
-        json = {json},
-        typescriptreact = {prettier, eslint},
-        vue = {prettier, eslint},
-        markdown = {markdown},
-        python = {
-            function()
-                return {exe = "black", args = {"-"}, stdin = true}
-            end
-        },
-        lua = {
-            -- luafmt
-            function()
-                return {exe = "lua-format", args = {"-i"}, stdin = true}
-            end
-        },
-        php = {
-            function()
-                return {exe = "php-cs-fixer", args = {"fix"}, stdin = false}
-            end
-        }
-    }
-})
-
-vim.api.nvim_exec([[
-  augroup FormatAutogroup
-    autocmd!
-    autocmd BufWritePost * FormatWrite
-  augroup END
-  autocmd BufWritePre * %s/\s\+$//e
-]], true)
+}
+local commonPrettier = {
+    "css", "scss", "html", "java", "javascript", "typescript",
+    "typescriptreact", "markdown", "markdown.mdx", "json"
+}
+for _, ft in ipairs(commonPrettier) do formatterConfig[ft] = {prettierConfig} end
+require('formatter').setup({logging = false, filetype = formatterConfig})
