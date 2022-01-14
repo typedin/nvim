a3e5b55875e4a4895cfdf1fda0ed77fe440748e4
Antoine Cheynet <antoine@typedin.dev>
2022-01-14 13:36:27 +0100

    updated lsp config for tailwind

--- lua/config-lsp/lsp-tailwind.lua
+++ lua/config-lsp/lsp-tailwind.lua
@@ -1,15 +1,45 @@
 require("lspconfig").tailwindcss.setup {
-    cmd = {
-        DATA_PATH ..
-            "/lspinstall/tailwindcss/node_modules/.bin/tailwindcss-language-server",
-        "--stdio"
-    },
+    cmd = {"tailwindcss-language-server", "--stdio"},
     filetypes = {
-        "blade", "vue", "html", "javascriptreact", "javascript.jsx",
-        "typescriptreact"
+        "aspnetcorerazor", "astro", "astro-markdown", "blade", "django-html",
+        "edge", "eelixir", "ejs", "erb", "eruby", "gohtml", "haml",
+        "handlebars", "hbs", "html", "html-eex", "jade", "leaf", "liquid",
+        "markdown", "mdx", "mustache", "njk", "nunjucks", "php", "razor",
+        "slim", "twig", "css", "less", "postcss", "sass", "scss", "stylus",
+        "sugarss", "javascript", "javascriptreact", "reason", "rescript",
+        "typescript", "typescriptreact", "vue", "svelte"
     },
-    -- LuaFormatter off
-    root_dir = require("lspconfig/util").root_pattern("package.json",
-                                                      "tsconfig.json",
-                                                      "jsconfig.json", ".git")
+    init_options = {userLanguages = {eelixir = "html-eex", eruby = "erb"}},
+    on_new_config = function(new_config)
+        if not new_config.settings then new_config.settings = {} end
+        if not new_config.settings.editor then
+            new_config.settings.editor = {}
+        end
+        if not new_config.settings.editor.tabSize then
+            -- set tab size for hover
+            new_config.settings.editor.tabSize =
+                vim.lsp.util.get_effective_tabstop()
+        end
+    end,
+    root_dir = require("lspconfig/util").root_pattern('tailwind.config.js',
+                                                      'tailwind.config.ts',
+                                                      'postcss.config.js',
+                                                      'postcss.config.ts',
+                                                      'package.json',
+                                                      'node_modules', '.git'),
+    settings = {
+        tailwindCSS = {
+            classAttributes = {"class", "className", "classList", "ngClass"},
+            lint = {
+                cssConflict = "warning",
+                invalidApply = "error",
+                invalidConfigPath = "error",
+                invalidScreen = "error",
+                invalidTailwindDirective = "error",
+                invalidVariant = "error",
+                recommendedVariantOrder = "warning"
+            },
+            validate = true
+        }
+    }
 }
