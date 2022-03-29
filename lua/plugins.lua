e5fdfe8cbbbdd5834b7af24e0871bd312f101d19
Antoine Cheynet <antoine@typedin.dev>
2022-03-29 17:25:50 +0200

    nvim tree

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -87,9 +87,6 @@ return require("packer").startup(function(use)
         requires = {
             "kyazdani42/nvim-web-devicons", -- optional, for file icon
         },
-        config = function()
-            require("nvim-tree").setup {}
-        end,
     }
     use "tpope/vim-projectionist"
     use "andymass/vim-matchup"
